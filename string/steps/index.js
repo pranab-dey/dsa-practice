f670c4e9a5e807e666481c75441378de71f02dc3
pranab-dey <pranab.strings07@gmail.com>
2025-09-10 12:49:03 +0600

    Add steps-recursion

--- string/steps/index.js
+++ string/steps/index.js
@@ -1,3 +1,5 @@
+// Solution #1
+
 function steps(n) {
 	for (let row = 0; row < n; row++) {
 		let stair = '';
@@ -17,3 +19,21 @@ console.log(steps(5));
 // '###  '
 // '#### '
 // '#####'
+
+// Solution #2
+
+function stepsRecursion(n, row = 0, stair = '') {
+	if (n === row) return;
+
+	if (stair.length === n) {
+		console.log(`'${stair}'`);
+		return stepsRecursion(n, row + 1);
+	}
+
+	if (stair.length <= row) stair += '#';
+	else stair += ' ';
+
+	return stepsRecursion(n, row, stair);
+}
+
+console.log(stepsRecursion(5));
